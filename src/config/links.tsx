8e864a32e5c6a2b03de0065dfb4d9dda90d84281
TouchKot723 <192498178+1987Shiz321@users.noreply.github.com>
2026-04-30 12:37:03 +0900

    Update: 外部リンクのアイコンを変更

--- src/config/links.tsx
+++ src/config/links.tsx
@@ -1,6 +1,5 @@
 import { AiOutlineInfoCircle } from 'react-icons/ai';
-import { FiTag } from 'react-icons/fi';
-import { GoHome } from 'react-icons/go';
+import { FiTag, FiExternalLink } from 'react-icons/fi';
 import { MdOutlineArticle } from 'react-icons/md';
 import { LinkType } from '@/types/link';
 
@@ -23,7 +22,7 @@ export const links: LinkType[] = [
   {
     name: 'ZAGARO-HP',
     href: 'https://uos-zagaro-eng.github.io/zagaro-hp/',
-    icon: <GoHome />,
+    icon: <FiExternalLink />,
     isExternal: true,
   },
 ];
